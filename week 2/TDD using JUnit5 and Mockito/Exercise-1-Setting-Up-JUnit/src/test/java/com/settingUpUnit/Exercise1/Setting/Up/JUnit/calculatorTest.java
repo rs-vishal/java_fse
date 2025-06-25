0e3bd6e822a13ee832c5faa1f321154e6d8d6d62
rs-vishal <vishalrs.22aid@kongu.edu>
2025-06-25 22:16:19 +0530

    junit

--- week 2/TDD using JUnit5 and Mockito/Exercise-1-Setting-Up-JUnit/src/test/java/com/settingUpUnit/Exercise1/Setting/Up/JUnit/calculatorTest.java
+++ week 2/TDD using JUnit5 and Mockito/Exercise-1-Setting-Up-JUnit/src/test/java/com/settingUpUnit/Exercise1/Setting/Up/JUnit/calculatorTest.java
@@ -1,18 +1,37 @@
 package com.settingUpUnit.Exercise1.Setting.Up.JUnit;
-
-
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
     private  final  Calculator cal = new Calculator();
-    
+
     @Test
     void testAdd() {
         int result = cal.add(2, 3);
-        assertEquals(5, result);
+        assertEquals(5, result);    
         assertNotEquals(3, result);
     }
+
+    @Test
+    void testSubtract() {
+        int result = cal.subtract(5, 3);
+        assertEquals(2, result);
+        assertNotEquals(3, result);
+    }
+
+    @Test
+    void testMultiply() {
+        int result = cal.multiply(2, 3);
+        assertEquals(6, result);
+        assertNotEquals(5, result);
+    }
+
+    @Test
+    void testDivide() {
+        int result = cal.divide(6, 3);
+        assertEquals(2, result);
+        assertNotEquals(3, result);
+    }
+
 }
