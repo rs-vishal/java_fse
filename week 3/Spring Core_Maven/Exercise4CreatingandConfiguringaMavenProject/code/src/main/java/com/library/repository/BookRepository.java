33b6d795a658fb0761013a2e6ee4bfd91d3ef83c
rs-vishal <vishalrs.22aid@kongu.edu>
2025-07-03 09:16:04 +0530

    week 3

--- week 3/Spring Core_Maven/Exercise4CreatingandConfiguringaMavenProject/code/src/main/java/com/library/repository/BookRepository.java
+++ week 3/Spring Core_Maven/Exercise4CreatingandConfiguringaMavenProject/code/src/main/java/com/library/repository/BookRepository.java
@@ -1,5 +1,10 @@
 package com.library.repository;
+
+
 public class BookRepository {
+    public BookRepository(){
+        System.out.println("BookRepository created");
+    }
     public void saveBook(String bookName) {
         System.out.println("Book saved: " + bookName);
     }
--- week 3/Spring Core_Maven/Exercise4CreatingandConfiguringaMavenProject/code/src/main/java/com/library/service/BookService.java
+++ week 3/Spring Core_Maven/Exercise4CreatingandConfiguringaMavenProject/code/src/main/java/com/library/service/BookService.java
@@ -1,8 +1,11 @@
 package com.library.service;
 
+
+
 import com.library.repository.BookRepository;
 
 public class BookService {
+        
         private BookRepository bookRepository;
 
         public BookService(BookRepository bookRepository) {
