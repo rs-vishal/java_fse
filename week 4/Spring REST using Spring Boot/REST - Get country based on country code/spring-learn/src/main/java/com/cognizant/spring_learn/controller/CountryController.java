b1b9345406ac373cbe502b489e6bab36a504acf5
rs-vishal <vishalrs.22aid@kongu.edu>
2025-07-12 08:54:54 +0530

    week 4

--- week 4/Spring REST using Spring Boot/REST - Get country based on country code/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
+++ week 4/Spring REST using Spring Boot/REST - Get country based on country code/spring-learn/src/main/java/com/cognizant/spring_learn/controller/CountryController.java
@@ -1,23 +1,21 @@
 package com.cognizant.spring_learn.controller;
 
-import com.cognizant.spring_learn.model.Country;
-import com.cognizant.spring_learn.service.CountryService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.cognizant.spring_learn.Country;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/country")
 public class CountryController {
 
-
-    @Autowired
-    private CountryService countryService;
-
-    @GetMapping("/country/{code}")
-    public Country getCountryIndia(@PathVariable  String code) {
-        return  countryService.getCountry(code);
+    @GetMapping("/{code}")
+    public Country getCountryByCode(@PathVariable String code) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("country.xml");
+        Country country = (Country) context.getBean(code.toLowerCase(), Country.class);
+        return country;
     }
 }
--- week 4/Spring REST using Spring Boot/REST - Get country based on country code/spring-learn/src/main/java/com/cognizant/spring_learn/repository/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cognizant.spring_learn.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.cognizant.spring_learn.model.Country;
-import java.util.Optional;
-
-public interface CountryRepository  extends JpaRepository<Country , String> {
-    Optional<Country> findByCodeIgnoreCase(String code);
-}
